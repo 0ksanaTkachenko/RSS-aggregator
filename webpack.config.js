82e9d879555f150b730ee01c7fc7abdd9f03bcf3
Oksana <ksenia.tkachenko.ua@gmail.com>
2024-09-27 12:30:47 +0700

    deleted vscode/settings.json

--- webpack.config.js
+++ webpack.config.js
@@ -1,71 +1,5 @@
 // // Generated using webpack-cli https://github.com/webpack/webpack-cli
 
-// const path = require('path');
-// const HtmlWebpackPlugin = require('html-webpack-plugin');
-
-// const isProduction = process.env.NODE_ENV === 'production';
-
-// const config = {
-//   entry: './src/index.js',
-//   output: {
-//     path: path.resolve(__dirname, 'dist'),
-//   },
-//   devServer: {
-//     open: true,
-//     host: 'localhost',
-//   },
-//   plugins: [
-//     new HtmlWebpackPlugin({
-//       template: 'index.html',
-//     }),
-
-//     // Add your plugins here
-//     // Learn more about plugins from https://webpack.js.org/configuration/plugins/
-//   ],
-//   module: {
-//     rules: [
-//       {
-//         test: /\.(js|jsx)$/i,
-//         loader: 'babel-loader',
-//         exclude: /node_modules/,
-//       },
-//       {
-//         test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-//         type: 'asset',
-//       },
-//       {
-//         test: /\.scss$/i,
-//         use: [
-//           'style-loader', // Вставляет CSS в DOM
-//           'css-loader', // Интерпретирует @import и url() как import/require()
-//           'sass-loader', // Компилирует Sass в CSS
-//         ],
-//       },
-
-//       // Add your rules for custom modules here
-//       // Learn more about loaders from https://webpack.js.org/loaders/
-//     ],
-//   },
-//   resolve: {
-//     extensions: ['.js'], // Здесь добавляются расширения по умолчанию
-//   },
-// };
-
-// module.exports = () => {
-//   if (isProduction) {
-//     config.mode = 'production';
-//   } else {
-//     config.mode = 'development';
-//   }
-//   return config;
-// };
-
-// Generated using webpack-cli https://github.com/webpack/webpack-cli
-
-// Generated using webpack-cli https://github.com/webpack/webpack-cli
-
-// Generated using webpack-cli https://github.com/webpack/webpack-cli
-
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
@@ -80,7 +14,7 @@ const config = {
     open: true,
     host: 'localhost',
     client: {
-      overlay: false, // Убирает предупреждения на экране
+      overlay: false,
     },
   },
   plugins: [
@@ -108,7 +42,6 @@ const config = {
             loader: 'sass-loader',
             options: {
               sassOptions: {
-                // Включи это, чтобы игнорировать предупреждения
                 quietDeps: true,
               },
             },
