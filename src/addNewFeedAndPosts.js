3af9e2e4313b0d6444f33f42802d3de6318b9777
Oksana <ksenia.tkachenko.ua@gmail.com>
2024-09-30 18:55:31 +0700

    Simplified state management: store only current feeds and posts. Temporary data (removed and new posts) are not persisted.

--- src/addNewFeedAndPosts.js
+++ src/addNewFeedAndPosts.js
@@ -1,15 +1,37 @@
 import createWatchedState from './view/view.js';
 
+// export const addNewFeed = (newFeedsData, postsState) => {
+//   const postsObservedState = createWatchedState(postsState);
+//   const existingFeedsArr = Array.from(postsState.feeds.existingFeeds);
+
+//   console.log(newFeedsData);
+
+//   if (existingFeedsArr.length === 0) {
+//     postsObservedState.feeds.existingFeeds.add(newFeedsData);
+//     return;
+//   }
+
+//   const isDuplicate = existingFeedsArr.some((feed) => feed.feedTitle === newFeedsData.feedTitle);
+
+//   if (!isDuplicate) {
+//     postsObservedState.feeds.existingFeeds.add(newFeedsData);
+//   }
+// };
+
 export const addNewFeed = (newFeedsData, postsState) => {
   const postsObservedState = createWatchedState(postsState);
   const existingFeedsArr = Array.from(postsState.feeds.existingFeeds);
+
+  console.log(newFeedsData);
 
   if (existingFeedsArr.length === 0) {
     postsObservedState.feeds.existingFeeds.add(newFeedsData);
     return;
   }
 
-  const isDuplicate = existingFeedsArr.some((feed) => feed.feedTitle === newFeedsData.feedTitle);
+  const newFeedTitle = newFeedsData.feedTitle.trim(); // Удаляем пробелы
+
+  const isDuplicate = existingFeedsArr.some((feed) => feed.feedTitle.trim() === newFeedTitle);
 
   if (!isDuplicate) {
     postsObservedState.feeds.existingFeeds.add(newFeedsData);
