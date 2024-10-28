1ba4659b37fbfcd0ca06a0f14e91af4498107192
Oksana <ksenia.tkachenko.ua@gmail.com>
2024-10-28 16:41:48 +0700

    feat: add URL uniqueness validation to link schema with `notOneOf` in yup

--- src/validate.js
+++ src/validate.js
@@ -1,25 +1,3 @@
-// import * as yup from 'yup';
-
-// const linkSchema = yup.object({
-//   url: yup
-//     .string()
-//     .min(1, 'EMPTY_URL')
-//     .url('INVALID_URL')
-//     .notOneOf(yup.ref('$existingLinks') || [], 'DUPLICATE_URL'),
-// });
-
-// const validateUrl = (urlValue, state) => {
-//   const feedsArr = Array.from(state.feeds);
-//   const existingLinks = feedsArr.map((item) => item.feedUrl);
-
-//   return linkSchema
-//     .validate({ url: urlValue }, { context: { existingLinks } })
-//     .then(() => ({ valid: true, message: 'URL_VALID' }))
-//     .catch((err) => {
-//       throw new Error(err.message);
-//     });
-// };
-
 import * as yup from 'yup';
 
 const createLinkSchema = (existingLinks) => {
@@ -28,15 +6,13 @@ const createLinkSchema = (existingLinks) => {
       .string()
       .min(1, 'EMPTY_URL')
       .url('INVALID_URL')
-      .notOneOf(existingLinks, 'DUPLICATE_URL'), // используем existingLinks напрямую
+      .notOneOf(existingLinks, 'DUPLICATE_URL'),
   });
 };
 
 const validateUrl = (urlValue, state) => {
   const feedsArr = Array.from(state.feeds);
   const existingLinks = feedsArr.map((item) => item.feedUrl);
-
-  // создаем схему, передавая existingLinks
   const linkSchema = createLinkSchema(existingLinks);
 
   return linkSchema
